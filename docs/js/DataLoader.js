1ca6b34d53bc64c595fd5fb8a4b8c27d5b663c9b
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2018-05-06 13:57:57 +0200

    Add controls for own data.

--- docs/js/DataLoader.js
+++ docs/js/DataLoader.js
@@ -1,36 +1,22 @@
 function DataLoader() {
-
-	var that = this;
-
-	/**
-	 * Load text file
-	 */
-	$("#speed_chart").text("Please wait... data are being loaded. This may take a while.");
-
 	this.loadData = function () {
+		const arrays = {
+			pts: [],
+			year: [],
+			month: [],
+			monthDay: [],
+			weekDay: [],
+			hour: [],
+			age: [],
+			sex: []
+		}
 
-		var pts = [];
-		var route_type = [];
-		var built_up_buffer = [];
-		var departure_hour = [];
-		var route_short_name = [];
+		let j = 0;
 
+		const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+		const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+		const sex = ['Male', 'Female'];
 
-		var weekday = new Array(7);
-		weekday[0] = "Sun";
-		weekday[1] = "Mon";
-		weekday[2] = "Tue";
-		weekday[3] = "Wed";
-		weekday[4] = "Thu";
-		weekday[5] = "Fri";
-		weekday[6] = "Sat";
-		var weekarray = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-
-		var j = 0;
-		/**
-		 * load data
-		 */
 		d3.csv('data/municipalities.csv', function (error, data) {
 			let municipalities = {};
 
@@ -40,46 +26,22 @@ function DataLoader() {
 
 			d3.csv('data/trafficAccidents100.csv', function (error, trafficAccidents) {
 				trafficAccidents.forEach(function (val, i) {
-					pts[j++] = municipalities[val.municipality].longitude * 0.71 + 128;
-					pts[j++] = -municipalities[val.municipality].latitude * 1.12 + 142.83;
+					arrays.pts[j++] = municipalities[val.municipality].longitude * 0.71 + 128;
+					arrays.pts[j++] = -municipalities[val.municipality].latitude * 1.12 + 142.83;
 
-					departure_hour[i] = val.hour;
-					route_type[i] = '3';//val.route_type;
-					built_up_buffer[i] = '15.2';//val.built_up__buffer;
-					if (val.route_type == "0") {
-						route_short_name[i] = val.route_short_name;
-					} else {
-						route_short_name[i] = "99";
-					}
+					arrays.year[i] = val.year;
+					arrays.month[i] = months[parseInt(val.month) - 1];
+					arrays.monthDay[i] = val.monthDay;
+					arrays.weekDay[i] = weekDays[parseInt(val.weekDay) - 1];
+					arrays.hour[i] = val.hour;
+					arrays.age[i] = val.age;
+					arrays.sex[i] = sex[parseInt(val.sex) - 1];
 				});
 
-				visualize({
-					pts: pts,
-					departure_hour: departure_hour,
-					route_type: route_type,
-					built_up_buffer: built_up_buffer,
-					num: trafficAccidents.length,
-					route_short_name: route_short_name
-				});
+				arrays.num = trafficAccidents.length;
+
+				visualize(arrays);
 			});
 		});
 	}
-
-
-	function getMinMax(val, minmax) {
-		if (typeof (minmax) == 'undefined') {
-			minmax = [];
-			minmax.min = Number.MAX_VALUE;
-			minmax.max = Number.MIN_VALUE;
-		}
-		if (val < minmax.min) {
-			minmax.min = val
-		};
-		if (val > minmax.max) {
-			minmax.max = val
-		};
-		return minmax;
-
-	}
-
 }
--- docs/js/init.js
+++ docs/js/init.js
@@ -1,195 +1,217 @@
+function init() {
+	initMap();
+
+	new DataLoader().loadData();
+}
+
+function visualize(data) {
+	//initialize WGL with link to data, the relative path to the shader folder, and id of the map div
+	WGL.init(data.num, '', 'map');
+
+	// map is global variable from Open Layers, we set our onMove 
+	// function to be called any time the map moves 
+	map.events.register("move", map, onMove);
+
+	// Adding heatmap, point map and polybrush interactions
+	var heatmap = WGL.addHeatMapDimension(data.pts, 'heatmap');
 	
-function init() { 
-		/*Initialize the map*/
-		initMap();
-		
-		/*Load the data*/
-		var data = new DataLoader();
-		data.loadData();			
+	//define radius function
+	WGL.addColorFilter('heatmap', 'colorbrush');
+	WGL.addPolyBrushFilter('heatmap', 'polybrush');
+
+	heatmap.renderer.colors.set([
+		1, 0, 1, 1,
+		1, 0, 1, 1,
+		0, 1, 1, 1,
+		0, 0, 0, 1
+	]);
+
+	heatmap.radiusFunction = function (r, z) {
+		var res = r / 20000 * Math.pow(2, z);
+		var gpsize = map.getGeodesicPixelSize();
+		var pixelsize = (gpsize.h + gpsize.w) / 2;
+		return res;
+	};
+	heatmap.setRadius(40);
+
+	WGL.addExtentFilter();
+
+	// Configuring the histograms and charts
+	var charts = [];
+	var params = [];
+	params.w = 640;
+	params.h = 180;
+	params.margin = {
+		top: 20,
+		right: 20,
+		bottom: 50,
+		left: 60
+	};
+
+	var d = [];
+
+	const histogram = function (config) {
+		const id = 'ch' + (d.length + 1);
+		const xlabel = config.label;
+		const name = config.name + 'F';
+		const chart = new WGL.ChartDiv('right', id, config.label);
+		if (config.domain) {
+			chart.setDim(WGL.addOrdinalHistDimension(config));
+		} else {
+			config.num_bins = config.max - config.min;
+			WGL.addLinearHistDimension(config);
+		}
+		WGL.addLinearFilter(config, config.max, name);
+		charts[config.name] = new WGL.ui.StackedBarChart(config, id, xlabel, name);
+		d.push(config);
 	}
 
-var link_ids =["11",
-              "6",
-              "5",
-              "2",
-              "4",
-              "15",
-              "7",
-              "10",
-              "3",
-              "9",
-              "12",
-              "24",
-              "8"]
-function visualize(data){	
-	
-		/**
-		 * initialize WGL with link to data, the relative path to the shader folder, and id of the map div
-		 */
-		
-		WGL.init(data.num,'', 'map');		
-		
-		/**
-		 * map is global variable from Open Layers, we set our onMove 
-		 * function to be called any time the map moves 
-		 */		 
-		map.events.register("move", map, onMove);							
-			
-		/**
-		 * Adding heatmap, point map and polybrush interactions
-		 */
-		var heatmap = WGL.addHeatMapDimension(data.pts, 'heatmap');
-		//define radius function
-			
-		//var mapdim = WGL.addMapDimension(data.pts, 'themap');
-		WGL.addColorFilter('heatmap','colorbrush');
-		WGL.addPolyBrushFilter('heatmap','polybrush');
+	histogram({
+		data: data.year,
+		min: 1995,
+		max: 2017,
+		name: 'year',
+		type: 'linear',
+		label: 'Year'
+	});
 
-		heatmap.renderer.colors.set([ 1, 0, 1, 1, 
-		           		              1, 0, 1, 1, 
-		        		              0, 1, 1, 1,
-		        		              0, 0, 0, 1 ]);
-		
-		heatmap.radiusFunction = function(r, z){			
-			var res = r/20000 * Math.pow(2,z);
-			//console.log(res);
-			var gpsize = map.getGeodesicPixelSize();
-			var pixelsize = (gpsize.h+gpsize.w)/2;
-			return  res ;
-			};
-			heatmap.setRadius(40);
-			
-		
-		WGL.addExtentFilter();
-		
-	
-		/**
-		 * Configuring the histograms and charts
-		 */
-		var charts = [];
-		var params = [];
-		params.w = 640;
-		params.h = 180;
-		params.margin = {
-			top : 20,
-			right : 20,
-			bottom : 50,
-			left : 60
-			};
-		/** Histogram for types */
-	//	var routetype   = {data: data.route_type,  domain:  ["0","2", "3"] ,  name: 'rt', type:'ordinal',  label : "Route type"};	
-	//	var chd1 = new WGL.ChartDiv("right","ch1", "Route type");
-	//	WGL.addOrdinalHistDimension( routetype);
-	//	WGL.addLinearFilter(routetype, 3 , 'rtf');
-	//	charts['rt']   = new  WGL.ui.StackedBarChart(routetype, "ch1", "route number","rtf");
-		
-		/** Histogram for hours*/
-		var hours = {data: data.departure_hour,  min:0, max:24, num_bins: 24, name: 'hours',type:'linear', label : "Hour of departure"} ;
-		var chd2 = new WGL.ChartDiv("right","ch2", "Hour of departure");
-		WGL.addLinearHistDimension(hours);			
-		WGL.addLinearFilter(hours, 24, 'hoursF');
-		charts['hours'] = new  WGL.ui.StackedBarChart(hours, "ch2", "hour of the day","hoursF");
-		
-		/** buildup area*/
-		var bup_area = {data: data.built_up_buffer,  min:0, max:100, num_bins: 50, name: 'bua',type:'linear', label : "Built up area (%)"} ;
-		var chd3 = new WGL.ChartDiv("right","ch3", "Built up area (%)");
-		WGL.addLinearHistDimension(bup_area);			
-		WGL.addLinearFilter(bup_area, 100, 'buaF');
-		charts['bua'] = new  WGL.ui.StackedBarChart(bup_area, "ch3", "built up area [%]","buaF");
-		
-		/** Histogram for ids */
-		var links   = {data: data.route_short_name,  domain:  link_ids ,  name: 'links', type:'ordinal', label : "Link id" };	
-		var chd4 = new WGL.ChartDiv("right","ch4", "Link id");
-		WGL.addOrdinalHistDimension( links );
-		WGL.addLinearFilter(links , 14 , 'linkf');
-		charts['links']   = new  WGL.ui.StackedBarChart(links, "ch4", "Link id","linkf");
-		
-		var d =[];
-		//d[0]= routetype;
-		d[0] = links;		
-		d[1] = hours;
-		d[2] = bup_area;	
-		
-	
-		var pc = WGL.addParallelCoordinates('pc_chart', d);
-		WGL.addMultiDim(d);
-		
-		//var legend = new WGL.ui.HeatMapLegend('heatlegend','colorbrush');
-		/**
-		 * Addin all charts
-		 */		
-		WGL.addCharts(charts);
-  	var controlHM = new WGL.ChartDiv("right","chm", "Heat map controls");
-  	addHeatMapControl(heatmap,'chm');
-		//WGL.addLegend(legend);
-		
-		/**
-		 * Initilizing all the filters
-		 */
-		WGL.initFilters();
-		
-		/** Drawing the map fist time */
-		WGL.mcontroller.zoommove(map.getZoom(), getTopLeftTC());
+	histogram({
+		data: data.month,
+		name: 'month',
+		type: 'ordinal',
+		label: 'Month',
+		domain: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+	});
+
+	histogram({
+		data: data.weekDay,
+		name: 'weekDay',
+		type: 'ordinal',
+		label: 'Day of the week',
+		domain: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+	});
+
+	histogram({
+		data: data.hour,
+		min: 0,
+		max: 24,
+		name: 'hour',
+		type: 'linear',
+		label: 'Hour'
+	});
+
+	histogram({
+		data: data.age,
+		min: 0,
+		max: 114,
+		name: 'age',
+		type: 'linear',
+		label: 'Age'
+	});
+
+	histogram({
+		data: data.sex,
+		name: 'sex',
+		type: 'ordinal',
+		label: 'Sex',
+		domain: ['Male', 'Female']
+	});
+
+	var pc = WGL.addParallelCoordinates('pc_chart', d);
+	WGL.addMultiDim(d);
+
+	//var legend = new WGL.ui.HeatMapLegend('heatlegend','colorbrush');
+	/**
+	 * Addin all charts
+	 */
+	WGL.addCharts(charts);
+	var controlHM = new WGL.ChartDiv("right", "chm", "Heat map controls");
+	addHeatMapControl(heatmap, 'chm');
+	//WGL.addLegend(legend);
+
+	/**
+	 * Initilizing all the filters
+	 */
+	WGL.initFilters();
+
+	/** Drawing the map fist time */
+	WGL.mcontroller.zoommove(map.getZoom(), getTopLeftTC());
+	WGL.render();
+
+
+	//var radius = 12.;	
+
+
+	$("#slider_radius").on("input", function () {
+		heatmap.setRadius(this.value);
+		//$('#radius_label').html(this.value+"m ");
+		//heatmap.reRender();
 		WGL.render();
-		
-		
-		//var radius = 12.;	
-		
-		
-		$("#slider_radius").on("input", function(){			
-			heatmap.setRadius(this.value);	
-			//$('#radius_label').html(this.value+"m ");
-			//heatmap.reRender();
-			WGL.render();			
-		});
-		
-		$("#slider_pc").on("input", function(){			
-			 //mapdim.render2(this.value);	
-			pc.reRender(this.value);		
-		});
-		
-		$("#points_visible").click(function(){
-			var l = WGL.getDimension(this.name);
-			l.setVisible(this.checked);					
-			WGL.render();			
-		});
-		$("#heatmap_visible").click(function(){
-			var l = WGL.getDimension(this.name);
-			l.setVisible(this.checked);
-			// heatmap.reRender();
-			WGL.render();			
-		});
-		
-		$("#pc_header").click(function(){
-			$(".pc_chart_div").slideToggle();
-			var l = WGL.getDimension("pc_chart");
-			
-			var resize =  function(){	
-			 	WGL.getManager().updateMapSize();
-			 	WGL.mcontroller.resize();	
-			 	WGL.mcontroller.zoommove(map.getZoom(), getTopLeftTC());
-			 	WGL.render();
-				}
-			if (l.visible){		
-					l.setVisible(false);
-					$('#map').animate({ 'margin-bottom': '1.5em'},
-							{done: resize})			
-					$('#pc').animate({ 'height': '1.5em'},
-									{done: resize})	
-									
-					$('#butPC').removeClass("fa-chevron-down");  
-					$('#butPC').addClass("fa-chevron-up");
-					setTimeout( function() { map.updateSize();}, 200);
-			} else {
-				l.setVisible(true);
-				$('#map').animate({ 'margin-bottom': '18.5em'},
-						{done: resize})		
-				$('#pc').animate({ 'height': '18.5em'},
-								{done: resize})	
-				$('#butPC').removeClass("fa-chevron-up");  
-				$('#butPC').addClass("fa-chevron-down");
-				setTimeout( function() { map.updateSize();}, 200);
-			}
+	});
+
+	$("#slider_pc").on("input", function () {
+		//mapdim.render2(this.value);	
+		pc.reRender(this.value);
+	});
+
+	$("#points_visible").click(function () {
+		var l = WGL.getDimension(this.name);
+		l.setVisible(this.checked);
+		WGL.render();
+	});
+	$("#heatmap_visible").click(function () {
+		var l = WGL.getDimension(this.name);
+		l.setVisible(this.checked);
+		// heatmap.reRender();
+		WGL.render();
+	});
+
+	$("#pc_header").click(function () {
+		$(".pc_chart_div").slideToggle();
+		var l = WGL.getDimension("pc_chart");
+
+		var resize = function () {
+			WGL.getManager().updateMapSize();
+			WGL.mcontroller.resize();
+			WGL.mcontroller.zoommove(map.getZoom(), getTopLeftTC());
+			WGL.render();
+		}
+		if (l.visible) {
+			l.setVisible(false);
+			$('#map').animate({
+				'margin-bottom': '1.5em'
+			}, {
+				done: resize
+			})
+			$('#pc').animate({
+				'height': '1.5em'
+			}, {
+				done: resize
+			})
+
+			$('#butPC').removeClass("fa-chevron-down");
+			$('#butPC').addClass("fa-chevron-up");
+			setTimeout(function () {
+				map.updateSize();
+			}, 200);
+		} else {
+			l.setVisible(true);
+			$('#map').animate({
+				'margin-bottom': '18.5em'
+			}, {
+				done: resize
+			})
+			$('#pc').animate({
+				'height': '18.5em'
+			}, {
+				done: resize
+			})
+			$('#butPC').removeClass("fa-chevron-up");
+			$('#butPC').addClass("fa-chevron-down");
+			setTimeout(function () {
+				map.updateSize();
+			}, 200);
+		}
 	});
 }
 
@@ -197,94 +219,91 @@ function visualize(data){
 /**
  * Function to calculate top left corner of the map in pixels for zoom 0
  * @returns {___anonymous_res}
- */	
+ */
 function getTopLeftTC() {
-	
+
 	var tlwgs = (new OpenLayers.LonLat(-180, 90)).transform(
-			new OpenLayers.Projection("EPSG:4326"),
-		 	new OpenLayers.Projection("EPSG:900913"));
-	
+		new OpenLayers.Projection("EPSG:4326"),
+		new OpenLayers.Projection("EPSG:900913"));
+
 	var s = Math.pow(2, map.getZoom());
 	tlpixel = map.getViewPortPxFromLonLat(tlwgs);
 	res = {
-			x : -tlpixel.x / s,
-			y : -tlpixel.y / s
+		x: -tlpixel.x / s,
+		y: -tlpixel.y / s
 	}
 	//console.log(res);
 	return res;
 }
-	
+
 /**
  * Function to for moving the map event.
  */
-function onMove() {			
-		WGL.mcontroller.zoommove(map.getZoom(), getTopLeftTC(), WGL.filterByExt);
+function onMove() {
+	WGL.mcontroller.zoommove(map.getZoom(), getTopLeftTC(), WGL.filterByExt);
 }
 
-function addHeatMapControl(hm,divid){
+function addHeatMapControl(hm, divid) {
 
-  $("#"+divid).append(
-    "<div id="+divid+"left style='top:0em; left:0em; width:40%'></div>"+
-    "<div id="+divid+"right style='top:0em; right:0em; width:35%; height:7em;'></div>");
+	$("#" + divid).append(
+		"<div id=" + divid + "left style='top:0em; left:0em; width:40%'></div>" +
+		"<div id=" + divid + "right style='top:0em; right:0em; width:35%; height:7em;'></div>");
 
 
-  var thediv = $("#"+divid+"right");
-  thediv.append(
-    "<div style='margin:1.2em 0.5em 0.5em 0.5em'>"+
-    "<text>Radius: </text><text id='radius_label'></text>"+
-    "<input style='width: 50%; right:1em; position:absolute' type ='range' max='2000' min='200'"+
-    "step='100' name='points' id='slider_radius' value='1000'></input> " +
-    "</div>");
-  thediv.append(
-    "<div style='margin:1.2em 0.5em 0.5em 0.5em'>"+
-    "<text>Density of records<br>within the radius: </text><text id='radius_label'></text>"+
-    "<div id='heatmap-legend' style='float: right'></div>"+
-    "</div>"
+	var thediv = $("#" + divid + "right");
+	thediv.append(
+		"<div style='margin:1.2em 0.5em 0.5em 0.5em'>" +
+		"<text>Radius: </text><text id='radius_label'></text>" +
+		"<input style='width: 50%; right:1em; position:absolute' type ='range' max='2000' min='200'" +
+		"step='100' name='points' id='slider_radius' value='1000'></input> " +
+		"</div>");
+	thediv.append(
+		"<div style='margin:1.2em 0.5em 0.5em 0.5em'>" +
+		"<text>Density of records<br>within the radius: </text><text id='radius_label'></text>" +
+		"<div id='heatmap-legend' style='float: right'></div>" +
+		"</div>"
 	);
-	
+
 	hm.setRadius(1000);
 	$('#radius_label').html('1000m');
 	WGL.render();
 
 
-  WGL.addColorFilter(hm.id,'colorbrush');
-  var legend = new  WGL.ui.HeatMapLegend("heatmap-legend", 'colorbrush', true);
-  hm.addLegend(legend);
-  WGL.addLegend(legend);
+	WGL.addColorFilter(hm.id, 'colorbrush');
+	var legend = new WGL.ui.HeatMapLegend("heatmap-legend", 'colorbrush', true);
+	hm.addLegend(legend);
+	WGL.addLegend(legend);
 
-  $("#slider_radius").on("input", function(){
+	$("#slider_radius").on("input", function () {
 
-    hm.setRadius(this.value);
+		hm.setRadius(this.value);
 
-    $('#radius_label').html(this.value+"m ");
-    //heatmap.reRender();
-    WGL.render();
-  });
+		$('#radius_label').html(this.value + "m ");
+		//heatmap.reRender();
+		WGL.render();
+	});
 
-  $("#cross").off("click");
-  $("#cross").click(function(e){
+	$("#cross").off("click");
+	$("#cross").click(function (e) {
 
-      $("#right").toggle();
-      $(this).toggleClass("active");
-      $("#sipka").toggleClass("fa-chevron-right");
-      $("#sipka").toggleClass("fa-chevron-left");
-      $("#map").toggleClass("fullscrean");
-      $("#pc").toggleClass("pc_chart_big");
+			$("#right").toggle();
+			$(this).toggleClass("active");
+			$("#sipka").toggleClass("fa-chevron-right");
+			$("#sipka").toggleClass("fa-chevron-left");
+			$("#map").toggleClass("fullscrean");
+			$("#pc").toggleClass("pc_chart_big");
 
-      WGL.getManager().updateMapSize();
-      WGL.mcontroller.resize();
-      WGL.mcontroller.zoommove(map.getZoom(),getTopLeftTC());
-      map.updateSize();
+			WGL.getManager().updateMapSize();
+			WGL.mcontroller.resize();
+			WGL.mcontroller.zoommove(map.getZoom(), getTopLeftTC());
+			map.updateSize();
 
-      $("#info").hide();
+			$("#info").hide();
 
-      WGL.getDimension("pc_chart").resize();
+			WGL.getDimension("pc_chart").resize();
 
-      WGL.render();
-    }
+			WGL.render();
+		}
 
-  )
+	)
 }
-	
-	
-	
